4e12908fd5a42d392914e3b83930591012426c78
gwiyeoni <bcwqcd@gmail.com>
2024-12-13 16:30:22 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/s20230490/w241213wBackendYoutubeAndSwim/W241213wBackendYoutubeAndSwimApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230490.w241213wBackendYoutubeAndSwim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W241213wBackendYoutubeAndSwimApplication
+
+fun main(args: Array<String>) {
+	runApplication<W241213wBackendYoutubeAndSwimApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20230490/w241213wBackendYoutubeAndSwim/W241213wBackendYoutubeAndSwimApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230490.w241213wBackendYoutubeAndSwim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W241213wBackendYoutubeAndSwimApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
